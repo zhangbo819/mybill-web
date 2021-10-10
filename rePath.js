01f16684647097b94cc0a4503791d0b267ab1bfb
张博 <zhangbo819@163.com>
2021-10-11 00:53:50 +0800

    fix

--- rePath.js
+++ rePath.js
@@ -3,10 +3,13 @@ const fs = require('fs')
 
 const targetFile = './docs/index.html'
 
+const host = 'https://zhangbo819.github.io/mybill-web/'
+
 fs.readFile(targetFile, 'utf-8', (err, data) => {
     console.log(err, data)
 
-    data = data.replace(/\/static\//ig, 'https://zhangbo819.github.io/mybill-web/static/')
+    data = data.replace(/\/static\//ig, host + 'static/')
+        .replace('/manifest.json', host + 'manifest.json')
 
-    fs.writeFile(targetFile, data, function () {})
+    fs.writeFile(targetFile, data, function () { })
 })
