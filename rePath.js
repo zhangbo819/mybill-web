fd49d052ae986246a8adfee29ff261c616fbcbed
张博 <zhangbo819@163.com>
2021-10-10 11:11:05 +0800

    feat

--- rePath.js
+++ rePath.js
@@ -1,1 +1,12 @@
-// TODO
+// TODO change build
+const fs = require('fs')
+
+const targetFile = './docs/index.html'
+
+fs.readFile(targetFile, 'utf-8', (err, data) => {
+    console.log(err, data)
+
+    data = data.replace('/static/', 'https://zhangbo819.github.io/mybill-web/static/')
+
+    fs.writeFile(targetFile, data, function () {})
+})
