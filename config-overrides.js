9af30a952212fd192f0bfa80cf31af00c8a9b722
张博 <zhangbo10@tal.com>
2021-10-12 16:12:23 +0800

    feat: eject

--- config-overrides.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-const paths = require("react-scripts/config/paths");
-paths.appBuild = path.join(path.dirname(paths.appBuild), "docs");
-config.output.path = path.join(path.dirname(config.output.path), "docs");
