d9af718cbba55be4d0de4301e8dff1c2ed9974a5
zhangbo <zhangbo@authing.cn>
2023-01-06 11:44:34 +0800

    feat: =

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ function App() {
   return (
     <div>
 
-      <InputBill storeKey="MBD" style={{ display: activeKey == 'home' ? 'block' : 'none' }} />
-      <InputBill storeKey="ACD" style={{ display: activeKey == 'home2' ? 'block' : 'none' }} />
+      <InputBill storeKey="MBD" style={{ display: activeKey === 'home' ? 'block' : 'none' }} />
+      <InputBill storeKey="ACD" style={{ display: activeKey === 'home2' ? 'block' : 'none' }} />
 
       <TabBar style={{ position: 'fixed', width: '100%', bottom: 0 }} activeKey={activeKey} onChange={setActiveKey}>
         {tabs.map(item => (
